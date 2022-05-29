0dcaa99e4b50fd45b93d7015ac779bbe8a656c5e
kaixiang wang <1419772184@qq.com>
2022-05-29 20:59:41 +0800

    done with Collatz

---
+++ lab4/flik/flikTest.java
@@ -0,0 +1,15 @@
+package flik;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class flikTest {
+
+    @Test
+    public void Flik() {
+
+        assertTrue(true);
+
+    }
+}
