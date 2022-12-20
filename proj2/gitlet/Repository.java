60c64a7c17bc183d614f8652ce8da095151f55cb
kaixiang wang <1419772184@qq.com>
2022-12-20 12:32:30 +0800

    20221220

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -237,8 +237,6 @@ public class Repository implements Serializable {
         File headBranch = join(HEADS, headFileString);
         //读取headBranch下的内容
         String headBranchText = Utils.readContentsAsString(headBranch);
-        System.out.println("headBranch"+headBranch);
-        System.out.println("headBranchText"+headBranchText);
         //根据commitId生成commit文件
         Commit parentCommit = Commit.fromFile(headBranchText);
         //判断删除暂存区是否存在，不存在就创建
@@ -305,8 +303,6 @@ public class Repository implements Serializable {
         File headBranch = join(HEADS, addFileString);
         //读取headBranch下的内容
         String headBranchText = Utils.readContentsAsString(headBranch);
-        System.out.println("headBranch:"+headBranch);
-        System.out.println("headBranchText:"+headBranchText);
         printLog(headBranchText);
     }
 
